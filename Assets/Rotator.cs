5f31ade7d7f7aef63c922270b0560dff5f66c494
Jaime Hernández <jaimevazz@gmail.com>
2018-07-05 16:02:49 -0500

    Initial commit

---
+++ Assets/Rotator.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotator : MonoBehaviour
+{
+    public float speed;
+
+    private Transform t;
+
+    private float angle = 0.0f;
+
+    public float Angle
+    {
+        get { return angle; }
+        set { angle = value; }
+    }
+
+    void Start () 
+    {
+        t = GetComponent<Transform>();
+	}
+	
+	void Update () 
+    {
+        angle = angle + speed * Time.deltaTime;
+        t.eulerAngles = new Vector3(0.0f, angle, 0.0f);
+	}
+}
