bf7f6c2cba6d4590b3a29e15c770f3041c38ba68
Jaime Hernandez <jaimevazz@gmail.com>
2018-07-08 08:56:12 -0500

    Added Items to map, MouseDownEvent and Utils

---
+++ Assets/_Claro/Scripts/Events/OnMouseDownEvent.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public class OnMouseDownEvent : MonoBehaviour 
+{
+	[SerializeField] private Canvas canvas;
+
+    public UnityEvent onMouseDownEvent;
+
+	private new bool enabled = false;
+	
+	void OnMouseDown () 
+	{
+		Debug.Log ("Click Events Invoked!");
+        this.onMouseDownEvent.Invoke();
+	}
+}
---
+++ Assets/_Claro/Scripts/Item.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Claro.Map
+{
+    public class Item : MonoBehaviour
+    {
+        [SerializeField] private Canvas canvas;
+
+        public void SetCurrent()
+        {
+            ItemManager.Instance.Current = this;
+        }
+
+        public void Display(bool display)
+        {
+            this.canvas.enabled = display;
+        }
+
+        void Start()
+        {
+            this.Display(false);
+        }
+
+    }
+}
+
+    
